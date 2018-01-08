4825ea7508d3d26d6878e4adc5b77628bf3398fe
yogeshborad <yogeshborad93@gmail.com>
2018-01-08 19:21:48 +0530

    simple todo

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React, {Component,} from 'react';
+import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {applyMiddleware,createStore} from 'redux';
+import todoApp from './reducers';
+import App from './components/App'
+
+let store =  createStore(todoApp)
+
+
+ReactDOM.render(
+    <Provider store= {store}>
+        <App />
+    </Provider>,
+document.getElementById('root'));
+
